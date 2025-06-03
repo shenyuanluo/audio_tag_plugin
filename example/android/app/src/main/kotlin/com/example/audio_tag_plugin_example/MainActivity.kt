df9f16984d9ad112a072d7b6e6bad890c37208eb
ShenYuanLuo <shenyuanluo@163.com>
2025-06-03 11:20:46 +0800

    feat: 新建工程代码
    1、feat: 新建工程代码

---
+++ example/android/app/src/main/kotlin/com/example/audio_tag_plugin_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.audio_tag_plugin_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/Classes/audio_tag_plugin.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../audio_tag_plugin.podspec for more information.
+#include "../../src/audio_tag_plugin.c"
